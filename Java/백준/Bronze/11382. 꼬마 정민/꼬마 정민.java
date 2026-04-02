7df2c9c29aa33158c4fbffe38f77c03059c7a603
pe-roo <mimi9166@naver.com>
2026-04-03 00:05:58 +0900

    [Bronze V] Title: 꼬마 정민, Time: 180 ms, Memory: 17644 KB -BaekjoonHub

---
+++ Java/백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		
+		long a = scanner.nextLong();
+		long b = scanner.nextLong();
+		long c = scanner.nextLong();
+		
+		System.out.println(a + b + c);
+		scanner.close();
+
+	}
+
+}
